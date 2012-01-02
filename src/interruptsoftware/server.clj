d55b3555c225dedbd2bbf6590a1df7425a911138
Timothy Washington <twashing@gmail.com>
2012-01-01 16:45:00 -0800

    -- init

---
+++ src/interruptsoftware/server.clj
@@ -0,0 +1,11 @@
+(ns interruptsoftware.server
+  (:require [noir.server :as server]))
+
+(server/load-views "src/interruptsoftware/views/")
+
+(defn -main [& m]
+  (let [mode (keyword (or (first m) :dev))
+        port (Integer. (get (System/getenv) "PORT" "8080"))]
+    (server/start port {:mode mode
+                        :ns 'interruptsoftware})))
+
---
+++ src/interruptsoftware/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns interruptsoftware.views.welcome
+  (:require [interruptsoftware.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to interruptsoftware"]))
