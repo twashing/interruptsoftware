4956b7556b915e57ddf5a86ff1e4042e692d5cbb
Timothy Washington <twashing@gmail.com>
2012-01-08 07:50:11 -0800

    -- defailt route now go to index.html -- introducing noir to the project -- using enlive as the server templating engine -- starting to build out the index page

--- src/interruptsoftware/views/welcome.clj
+++ src/interruptsoftware/views/welcome.clj
@@ -1,9 +1,19 @@
 (ns interruptsoftware.views.welcome
   (:require [interruptsoftware.views.common :as common]
-            [noir.content.getting-started])
+            [noir.content.getting-started]
+            [net.cgrand.enlive-html :as enlive])
   (:use [noir.core :only [defpage]]
-        [hiccup.core :only [html]]))
+        [hiccup.core :only [html]])
+)
 
 (defpage "/welcome" []
          (common/layout
            [:p "Welcome to Interrupt Software"]))
+
+(defpage "/" []
+  (let  [templ (enlive/html-resource "index.html")
+        ]
+    (enlive/emit* templ)
+  )
+)
+
