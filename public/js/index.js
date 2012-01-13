446675992b50e859db97e4ebfa089a9828fc907d
Timothy Washington <twashing@gmail.com>
2012-01-12 18:56:45 -0800

    -- adding page transitions onclick -- have a common function "transitionPageHandler" for transitions

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,32 @@
 (function() {
-  /* JQuery document ready handler */;  $(document).ready(function() {
+  var transitionPageHandler;
+  transitionPageHandler = function(ctxt, purl) {
+    var centreC, ctxtObj;
+    centreC = $("#centre-content");
+    ctxtObj = $(ctxt);
+    return ctxtObj.click(function() {
+      return centreC.fadeOut(200, "linear", function() {
+        return centreC.load(purl, function() {
+          return centreC.fadeIn(500, "linear");
+        });
+      });
+    });
+  };
+  /* JQuery document ready handler */;
+  $(document).ready(function() {
     /* 
       Load the index content 
-    */    $("#centre-content").load("/include/main.html");
+    */    $("#centre-content").css('display', 'none').load("/include/main.html").fadeIn(500, "linear");
     /*
         Add handlers for 
           1. right menu items and 
           2. main ribbon 
+      $("#item-about").click -> $("#centre-content").load("/include/profile.html")
+      $("#item-portfolio").click -> $("#centre-content").load("/include/portfolio.html")
+      $("#item-main , #main-ribbon").click -> $("#centre-content").load("/include/main.html")
       */
-    $("#item-about").click(function() {
-      return $("#centre-content").load("/include/profile.html");
-    });
-    $("#item-portfolio").click(function() {
-      return $("#centre-content").load("/include/portfolio.html");
-    });
-    return $("#item-main , #main-ribbon").click(function() {
-      return $("#centre-content").load("/include/main.html");
-    });
+    transitionPageHandler("#item-about", "/include/profile.html");
+    transitionPageHandler("#item-portfolio", "/include/portfolio.html");
+    return transitionPageHandler("#item-main , #main-ribbon", "/include/main.html");
   });
 }).call(this);
--- public/templ/coffeescript/index.coffee
+++ public/templ/coffeescript/index.coffee
@@ -1,20 +1,39 @@
 
+transitionPageHandler = (ctxt, purl) -> 
+  
+  centreC = $("#centre-content")
+  ctxtObj = $(ctxt)
+  
+  ctxtObj.click ->
+    centreC.fadeOut(200, "linear", () ->
+        centreC.load(purl, () -> 
+          centreC.fadeIn(500, "linear")
+        )
+      )
+
 /* JQuery document ready handler */ 
 $(document).ready -> 
+  
   
   ### 
     Load the index content 
   ###
-  $("#centre-content").load("/include/main.html")
+  $("#centre-content")
+    .css('display', 'none')
+    .load("/include/main.html")
+    .fadeIn(500, "linear")
   
   
   ###
     Add handlers for 
       1. right menu items and 
       2. main ribbon 
-  ###
   $("#item-about").click -> $("#centre-content").load("/include/profile.html")
   $("#item-portfolio").click -> $("#centre-content").load("/include/portfolio.html")
-  
   $("#item-main , #main-ribbon").click -> $("#centre-content").load("/include/main.html")
-
+  ###
+  
+  transitionPageHandler("#item-about", "/include/profile.html")
+  transitionPageHandler("#item-portfolio", "/include/portfolio.html")
+  transitionPageHandler("#item-main , #main-ribbon", "/include/main.html")
+  
