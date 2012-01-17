8f2ce1bd7d4bc64c7ef350af6f69d8619580df97
Timothy Washington <twashing@gmail.com>
2012-01-17 07:01:44 -0800

    -- trials at using Backbone's Router with the History API -- improving the favicon icon

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,14 @@
 (function() {
   /* Common handler for page transitions */;
   var transitionPageHandler;
-  transitionPageHandler = function(ctxt, purl) {
+  transitionPageHandler = function(ctxt, purl, proute) {
     var centreC, ctxtObj;
     centreC = $("#centre-content");
     ctxtObj = $(ctxt);
     return ctxtObj.click(function() {
       return centreC.fadeOut(200, "linear", function() {
         return centreC.load(purl, function() {
-          return centreC.fadeIn(500, "linear");
+          /* interruptRouter.navigate(proute) */;          return centreC.fadeIn(500, "linear");
         });
       });
     });
@@ -17,14 +17,36 @@
   $(document).ready(function() {
     /* 
       Load the index content 
-    */    $("#centre-content").css('display', 'none').load("/include/main.html").fadeIn(500, "linear");
+    */
+    var indexRoute, interruptRouter, portfolioRoute, profileRoute;
+    $("#centre-content").css('display', 'none').load("/include/main.html").fadeIn(500, "linear");
     /*
         Add handlers for 
           1. right menu items and 
           2. main ribbon 
       */
-    transitionPageHandler("#item-about", "/include/profile.html");
-    transitionPageHandler("#item-portfolio", "/include/portfolio.html");
-    return transitionPageHandler("#item-main , #main-ribbon", "/include/main.html");
+    transitionPageHandler("#item-about", "/include/profile.html", "/profile");
+    transitionPageHandler("#item-portfolio", "/include/portfolio.html", "/portfolio");
+    transitionPageHandler("#item-main , #main-ribbon", "/include/main.html", "/");
+    /*
+        Backbone Router URLs
+      */
+    interruptRouter = Backbone.Router.extend({
+      routes: {
+        "/": "indexRoute",
+        "/profile": "profileRoute",
+        "/portfolio": "portfolioRoute"
+      }
+    });
+    indexRoute = function() {
+      return $("#item-main").click();
+    };
+    profileRoute = function() {
+      return $("#item-about").click();
+    };
+    portfolioRoute = function() {
+      return $("#item-portfolio").click();
+    };
+    return /* Backbone.history.start( pushState: true) */;
   });
 }).call(this);
--- public/templ/coffeescript/index.coffee
+++ public/templ/coffeescript/index.coffee
@@ -1,6 +1,6 @@
 
 /* Common handler for page transitions */
-transitionPageHandler = (ctxt, purl) -> 
+transitionPageHandler = (ctxt, purl, proute) -> 
   
   centreC = $("#centre-content")
   ctxtObj = $(ctxt)
@@ -8,6 +8,8 @@ transitionPageHandler = (ctxt, purl) ->
   ctxtObj.click ->
     centreC.fadeOut(200, "linear", () ->
         centreC.load(purl, () -> 
+
+          /* interruptRouter.navigate(proute) */
           centreC.fadeIn(500, "linear")
         )
       )
@@ -29,7 +31,28 @@ $(document).ready ->
       2. main ribbon 
   ###
   
-  transitionPageHandler("#item-about", "/include/profile.html")
-  transitionPageHandler("#item-portfolio", "/include/portfolio.html")
-  transitionPageHandler("#item-main , #main-ribbon", "/include/main.html")
+  transitionPageHandler("#item-about", "/include/profile.html", "/profile")
+  transitionPageHandler("#item-portfolio", "/include/portfolio.html", "/portfolio")
+  transitionPageHandler("#item-main , #main-ribbon", "/include/main.html", "/")
   
+  
+  ###
+    Backbone Router URLs
+  ###
+  interruptRouter = Backbone.Router.extend(
+    routes: 
+      "/" : "indexRoute"
+      "/profile" : "profileRoute"
+      "/portfolio" : "portfolioRoute"
+  )
+  
+  indexRoute = -> 
+    $("#item-main").click()
+  profileRoute = -> 
+    $("#item-about").click()
+  portfolioRoute = -> 
+    $("#item-portfolio").click()
+  
+  /* Backbone.history.start( pushState: true) */
+
+
