5469c2903a98ece59bc1ec7250308cce5a0fdd8d
Timothy Washington <twashing@interruptsoftware.com>
2016-10-29 11:32:07 -0700

    Adding a default route53 NS record Pulling from the latest docker image

--- aws.tf
+++ aws.tf
@@ -128,12 +128,25 @@ resource "aws_route53_zone" "main" {
   name = "interruptsoftware.com"
 }
 
-resource "aws_route53_record" "main-ns" {
+resource "aws_route53_record" "main-a" {
     zone_id = "${aws_route53_zone.main.zone_id}"
     name = "interruptsoftware.com"
     type = "A"
     ttl = "300"
     records = [
         "${aws_instance.interruptsoftware-instance.public_ip}"
+    ]
+}
+
+resource "aws_route53_record" "main-ns" {
+    zone_id = "${aws_route53_zone.main.zone_id}"
+    name = "interruptsoftware.com"
+    type = "NS"
+    ttl = "300"
+    records = [
+        "${aws_route53_zone.main.name_servers.0}",
+        "${aws_route53_zone.main.name_servers.1}",
+        "${aws_route53_zone.main.name_servers.2}",
+        "${aws_route53_zone.main.name_servers.3}"
     ]
 }
